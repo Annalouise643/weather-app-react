9c7dc1cdb745e77cae6430882fdfcc44fe8111fc
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-01-07 11:04:25 +0000

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>React Weather Search Engine</h1>
-      <form>
-        <input type="text" placeholder="enter a city" />
-        <input type="submit" value="Search" />
-        <button>Current</button>
-      </form>
+    <div class="container">
+      <div className="App card">
+        <h1>React Weather Search Engine</h1>
+
+        <form>
+          <input type="text" placeholder="enter a city" />
+          <input type="submit" value="Search" class="btn btn-primary" />
+          <button class="btn btn-success">Current</button>
+        </form>
+      </div>
     </div>
   );
 }
