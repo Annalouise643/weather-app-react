d0ab7c34bc34bf83c0a1395ae39aaba89070694c
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-01-07 09:48:03 +0000

    Removed search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
-import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <Search />
+      <h1>React Weather Search Engine</h1>
+      <form>
+        <input type="text" placeholder="enter a city" />
+        <input type="submit" value="Search" />
+        <button>Current</button>
+      </form>
     </div>
   );
 }
