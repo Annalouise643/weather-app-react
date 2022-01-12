bb86533d0d7a23f205305cddc193d0c766f88129
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-01-12 22:08:53 +0000

    Updates CSS of form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import "./App.css";
 
 function App() {
   return (
-    <div class="container">
-      <div className="App card">
+    <div className="container">
+      <div className="App card p-5">
         <h1>React Weather Search Engine</h1>
 
         <form>
-          <input type="text" placeholder="enter a city" />
-          <input type="submit" value="Search" class="btn btn-primary" />
-          <button class="btn btn-success">Current</button>
+          <div className="row">
+            <div className="col-8">
+              <input
+                type="text"
+                placeholder="Enter a city"
+                className="w-100"
+                autofocus
+              />
+            </div>
+
+            <div className="col-2">
+              <input type="submit" value="Search" class="btn btn-primary" />
+            </div>
+            <div className="col-2">
+              <button className="btn btn-success">Current</button>
+            </div>
+          </div>
         </form>
       </div>
     </div>
