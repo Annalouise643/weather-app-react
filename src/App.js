7beeb66d25063bebfc493251ed9bb6c467566c51
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-01-14 18:47:08 +0000

    Updated CSS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ function App() {
         <h1 className="mb-3">React Weather Search Engine</h1>
         <form>
           <div class="input-group mb-3">
-            <input type="text" placeholder="Enter a city" autofocus />
+            <input
+              type="text"
+              placeholder="Enter a city"
+              className="search-bar"
+              autofocus
+            />
             <input type="submit" value="Search" class="btn btn-primary" />
             <button className="btn btn-success">Current</button>
           </div>
@@ -15,7 +20,12 @@ function App() {
         <h4>London</h4>
         <p>Friday 17:49</p>
         <div className="row">
-          <div className="col">Clouds</div>
+          <div className="col">
+            <div className="row">
+              <div className="col">Clouds</div>
+              <div className="col">[Cloud Icon]</div>
+            </div>
+          </div>
           <div className="col temp">
             14<small>°C</small>
           </div>
