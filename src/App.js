015b0b1228985be5c18380191d8d878494eff068
Annalouise643 <72890896+Annalouise643@users.noreply.github.com>
2022-01-14 18:36:46 +0000

    Added current weather jsx and css

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className="container m-30">
       <div className="App card p-5">
         <h1 className="mb-3">React Weather Search Engine</h1>
         <form>
@@ -12,6 +12,18 @@ function App() {
             <button className="btn btn-success">Current</button>
           </div>
         </form>
+        <h4>London</h4>
+        <p>Friday 17:49</p>
+        <div className="row">
+          <div className="col">Clouds</div>
+          <div className="col temp">
+            14<small>°C</small>
+          </div>
+          <div className="col">
+            <p>Precipitation: 0%</p>
+            <p>Wind:3km/h</p>
+          </div>
+        </div>
       </div>
     </div>
   );
